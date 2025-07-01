69ea4900899e2b5e703b293d278581da7901a51b
NoNamedCobble <nexuspl500@gmail.com>
2025-07-01 18:24:14 +0200

    feat: add featured products section with slider

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ const toggleNav = () => {
 document
   .querySelectorAll(".header__mobile-nav-button, .header__mobile-nav-item")
   .forEach((button) => button.addEventListener("click", () => toggleNav()));
+
+document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.querySelector(".swiper-wrapper");
+
+  // duplicate existing slides
+  const slides = Array.from(wrapper.querySelectorAll(".swiper-slide"));
+  slides.forEach((slide) => {
+    const clone = slide.cloneNode(true);
+    wrapper.appendChild(clone);
+  });
+
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    loop: true,
+    scrollbar: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper__button--next",
+    },
+    breakpoints: {
+      1200: {
+        spaceBetween: 24,
+      },
+    },
+  });
+});
